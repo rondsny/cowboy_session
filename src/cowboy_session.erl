82a3b04be358d39c99c840eac7639389abad1881
Veiyn <rondsny@gmail.com>
2016-11-04 11:38:27 +0800

    Update cowboy_session.erl
    对cowboy2.0的支持

--- src/cowboy_session.erl
+++ src/cowboy_session.erl
@@ -86,22 +86,23 @@ init([]) ->
 
 get_session(Req) ->
 	Cookie_name = ?CONFIG(cookie_name),
-	{SID, Req2} = case cowboy_req:meta(Cookie_name, Req) of
-		{undefined, Req3} ->
-			cowboy_req:cookie(Cookie_name, Req3);
+	SID = case cowboy_req:meta(Cookie_name, Req) of
+		undefined ->
+			Map = cowboy_req:match_cookies(Req),
+			maps:get(Cookie_name, Map, undefined);
 		Result ->
 			Result
 	end,
 	case SID of
 		undefined ->
-			create_session(Req2);
+			create_session(Req);
 		_ ->
 			case gproc:lookup_local_name({cowboy_session, SID}) of
 				undefined ->
-					create_session(Req2);
+					create_session(Req);
 				Pid ->
 					cowboy_session_server:touch(Pid),
-					{Pid, Req2}
+					{Pid, Req}
 			end
 	end.
 
